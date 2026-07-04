e65cb1b15672678712b70a5015b3dee2baf5d7c8
djEjs <52478827+djEjs@users.noreply.github.com>
2026-07-05 04:41:26 +0900

    BuildClean

--- Assets/Editor/WebGLBuild.cs
+++ Assets/Editor/WebGLBuild.cs
@@ -4,6 +4,10 @@ public static class WebGLBuild
 {
     public static void Build()
     {
+        PlayerSettings.WebGL.compressionFormat = WebGLCompressionFormat.Gzip;
+        PlayerSettings.WebGL.decompressionFallback = true;
+        PlayerSettings.WebGL.dataCaching = true;
+
         BuildPipeline.BuildPlayer(
             new[] { "Assets/Scenes/SampleScene.unity" },
             "Build/WebGL",
